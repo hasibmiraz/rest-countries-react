11ba6a9978dd45fd7947b4c9d5ea4f80f6625967
hasibmiraz <hasibmiraz09@gmail.com>
2022-03-20 02:12:20 +0600

    Added region and area

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Country.css';
 
-const Country = ({ name, population }) => {
+const Country = ({ name, population, region, area }) => {
   return (
     <div className="country">
       <h2>Name: {name}</h2>
       <h4>Population: {population}</h4>
+      <p>
+        <small>Region: {region}</small>
+      </p>
+      <p>
+        <small>Area: {area}</small>
+      </p>
     </div>
   );
 };
